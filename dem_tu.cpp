32384775852a4b11fcc8718cca372cc62215758c
tinq1907 <153514252+tinq1907@users.noreply.github.com>
2024-03-20 11:47:54 +0700

    Update dem_tu.cpp

--- dem_tu.cpp
+++ dem_tu.cpp
@@ -7,7 +7,7 @@ using ull = unsigned long long;
 int main()
 {
     ios::sync_with_stdio(false); cin.tie(0);
-	cin.tie(NULL);
+    cin.tie(NULL);
     cout.tie(NULL);
     int t; cin >> t;
     cin.ignore();
