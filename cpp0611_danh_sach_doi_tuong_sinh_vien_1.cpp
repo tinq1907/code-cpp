b2b8b039084809f609cdf0a437cbf9470a077f85
tinq1907 <153514252+tinq1907@users.noreply.github.com>
2024-05-13 21:17:03 +0700

    Create cpp0611_danh_sach_doi_tuong_sinh_vien_1.cpp

---
+++ cpp0611_danh_sach_doi_tuong_sinh_vien_1.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ull = unsigned long long;
+using ll = long long;
+int cnt = 1;
+class SinhVien{
+    private:
+        string id, name, mclass, dob;
+        string d, m, y;
+        float gpa;
+    public:
+        friend void dob_stand(SinhVien &a);
+        friend istream &operator >> (istream &input, SinhVien &a);
+        friend ostream &operator << (ostream &output, SinhVien a);
+};
+
+void dob_stand(SinhVien &a){
+    stringstream ss(a.dob);
+    getline(ss, a.d, '/');
+    getline(ss, a.m, '/');
+    getline(ss, a.y);
+    if (a.d.size() == 1) a.d = "0" + a.d;
+    if (a.m.size() == 1) a.m = "0" + a.m;
+    a.dob = a.d + "/" + a.m + "/" + a.y;
+}
+
+istream &operator >> (istream &input, SinhVien &a){
+    cin.ignore();
+    if (cnt < 10){
+        a.id = "B20DCCN00" + to_string(cnt++);
+    }
+    else a.id = "B20DCCN0" + to_string(cnt++);
+    getline(cin, a.name);
+    getline(cin, a.mclass);
+    getline(cin, a.dob);
+    dob_stand(a);
+    cin >> a.gpa;
+    return input;
+}
+
+ostream &operator << (ostream &output, SinhVien a){
+    cout << a.id << " " << a.name << " " << a.mclass << " " << a.dob << " " << setprecision(2) << fixed << a.gpa;
+    cout << endl;
+    return output;
+}
+
+int main(){
+    SinhVien ds[50];
+    int N, i;
+    cin >> N;
+    for(i=0;i<N;i++){
+        cin >> ds[i];
+    }
+    for(i=0;i<N;i++){
+        cout << ds[i];
+    }
+    return 0;
+}
