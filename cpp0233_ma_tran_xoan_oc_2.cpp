3f2d7f7da2d7a25853d3ae1034841baec809be5a
Nguyen Quoc Thai <ThaiNQ.B23CN745@stu.ptit.edu.vn>
2024-06-02 21:11:15 +0700

    ADD CODEPTIT FILES

---
+++ cpp0233_ma_tran_xoan_oc_2.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; cin >> t;
+    while(t--)
+    {
+        int n, m; cin >> n >> m;
+        int l = 0, r = m - 1, u = 0, d = n - 1;
+        int c = (int)m * n;
+        int a[n][m], b[c];
+        int cnt = 0;
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < m; ++j) cin >> a[i][j];
+        }
+        while (l <= r && u <= d){
+            for (int i = l; i <= r; ++i){
+                b[cnt++] = a[u][i];
+                
+            }
+            u++;
+            for (int i = u; i <= d; ++i){
+                b[cnt++] = a[i][r];
+            }
+            r--;
+            if(u <= d){
+                for (int i = r; i >= l; --i){
+                    b[cnt++] = a[d][i];
+                }
+                d--;
+            }
+            if (l <= r){
+                for (int i = d; i >= u; --i){
+                    b[cnt++] = a[i][l];
+                }
+                l++;
+            }
+        }
+        for (int i = c - 1; i >= 0; i--){
+            cout << b[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
